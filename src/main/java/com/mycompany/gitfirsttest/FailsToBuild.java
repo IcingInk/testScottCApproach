f60f1ca1fb1243902ba8703966ef43edd941ba6c
IcingInk <inkimar.nrm@gmail.com>
2016-01-23 19:21:15 +0100

    should fail

--- src/main/java/com/mycompany/gitfirsttest/FailsToBuild.java
+++ src/main/java/com/mycompany/gitfirsttest/FailsToBuild.java
@@ -1,3 +1,5 @@
+package com.mycompany.gitfirsttest;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +11,5 @@
  * @author ingimar
  */
 public class FailsToBuild {
-    fails;
+    krasch;
 }
